1bb976e73ca1f0367581615c56d974b642a9319f
Lautaro Fernandez Szuter <lautaroszutner@gmail.com>
2025-04-17 19:56:12 -0300

    subo header

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="">
+      <div className="h-screen grid grid-cols-2 place-items-center">
+        <div className="text-start">
+          <p className="text-2xl font-bold">Lautaro Fernandez Szutner</p>
+          <p className="text-lg">Diseñador & Programador web</p>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
