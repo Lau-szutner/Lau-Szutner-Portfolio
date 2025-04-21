ffb381ab22a9292ee197146e6435acf39dd20649
Lau-szutner <lautaroszutner@gmail.com>
2025-04-21 19:15:09 -0300

    ajuste div;

--- src/components/About..tsx
+++ src/components/About..tsx
@@ -2,18 +2,23 @@ import Experience from './Experience';
 
 const About = () => {
   return (
-    <div className="w-full bg-blue-500 py-10">
+    <div className="w-full py-10">
       <div className="flex flex-col items-center">
-        <h2 className="text-3xl font-bold mb-8">About Me</h2>
-        <div className="flex flex-col gap-5 w-11/12 max-w-3xl">
+        <div className="grid grid-cols-2">
+          <div></div>
           <div>
-            <h3 className="font-bold text-2xl mb-3">Work experience</h3>
-            <Experience title="hola" subject="dlsllss" />
-            <Experience title="hola" subject="dlsllss" />
-          </div>
-          <div>
-            <h3 className="font-bold text-2xl mb-3">Education</h3>
-            <p className="text-white">[Agregar contenido educativo aquí]</p>
+            <h2 className="text-3xl font-bold mb-8">About Me</h2>
+            <div className="flex gap-5 max-w-3xl">
+              <div>
+                <h3 className="font-bold text-2xl mb-3">Work experience</h3>
+                <Experience title="hola" subject="dlsllss" />
+                <Experience title="hola" subject="dlsllss" />
+              </div>
+              <div>
+                <h3 className="font-bold text-2xl mb-3">Education</h3>
+                <p className="text-white">[Agregar contenido educativo aquí]</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
