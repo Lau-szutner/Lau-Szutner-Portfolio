070d7a0b4db3f02f5afb4a604c0136dcde45c052
Lautaro Fernandez Szuter <lautaroszutner@gmail.com>
2025-04-29 02:11:44 -0300

    effects2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import Button from './Button';
-
+import Tilt from 'react-parallax-tilt';
 function Header() {
   return (
     <header className="h-screen overflow-hidden background">
@@ -20,17 +20,35 @@ function Header() {
             <Button link={'https://github.com/Lau-szutner/portfolio'}>
               Github
             </Button>
+
+            <Button
+              link={
+                'https://ar.linkedin.com/in/lautaro-fernandez-szutner-203639277?original_referer=https%3A%2F%2Far.linkedin.com%2Fpub%2Fdir%2FLautaro%2FFernandez%2Far-0-Argentina'
+              }
+            >
+              Linkein
+            </Button>
+
+            <Button
+              link={
+                'https://ar.linkedin.com/in/lautaro-fernandez-szutner-203639277?original_referer=https%3A%2F%2Far.linkedin.com%2Fpub%2Fdir%2FLautaro%2FFernandez%2Far-0-Argentina'
+              }
+            >
+              Email
+            </Button>
           </div>
         </div>
 
         <div className="flex flex-col items-center gap-5 ">
-          <div className="line">
-            <img
-              src="/photo-perfil.png"
-              alt="Foto de perfil"
-              className="rounded-lg"
-            />
-          </div>
+          <Tilt tiltReverse={false} scale={1.2}>
+            <div className="line">
+              <img
+                src="/photo-perfil.png"
+                alt="Foto de perfil"
+                className="rounded-lg"
+              />
+            </div>
+          </Tilt>
         </div>
       </div>
     </header>
