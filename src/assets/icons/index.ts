bbeca10e122540845f1247374081452dc4fb965f
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-06-14 00:59:52 +0000

    php icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,12 +2,13 @@ import CssIcon from './CssIcon';
 import HtmlIcon from './HtmlIcon';
 import ReactIcon from './ReactIcon';
 import JavascriptIcon from './JavascriptIcon';
-
+import PhpIcon from './PhpIcon';
 const icons = {
   css: CssIcon,
   html: HtmlIcon,
   react: ReactIcon,
   javascript: JavascriptIcon,
+  php: PhpIcon,
   // agrega más aquí
 };
 
