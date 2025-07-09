d24cb6ed9170f946b7a57cfbc71475dbea76d525
Lau-szutner <lautaroszutner@gmail.com>
2025-07-09 20:42:44 -0300

    carrousel

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,7 +1,17 @@
 // import Project from './Project';
 import ProjectCard from './ProjectsCard';
+import { useEffect } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 
 const Projects = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()); // Access API
+    }
+  }, [emblaApi]);
+
   return (
     <section
       className="h-min-screen flex flex-col place-items-center gap-5 pt-30 "
@@ -10,52 +20,55 @@ const Projects = () => {
       <h2 className="text-4xl sm:text-6xl md:text-7xl font-bold text-center w-full mb-15">
         Projects
       </h2>
-
-      <ProjectCard
-        title={'MoneyMind'}
-        skills={[
-          // { iconName: 'css', label: 'CSS3' },
-          // { iconName: 'html', label: 'HTML5' },
-          { iconName: 'react', label: 'React' },
-          { iconName: 'javascript', label: 'Javascript' },
-          { iconName: 'typescript', label: 'Typescript' },
-          { iconName: 'nextjs', label: 'Nextjs' },
-          { iconName: 'sequelize', label: 'Sequelize' },
-          { iconName: 'git', label: 'Git' },
-          { iconName: 'nodejs', label: 'Nodejs' },
-          { iconName: 'express', label: 'Express' },
-        ]}
-      ></ProjectCard>
-      <ProjectCard
-        title={'MoneyMind'}
-        skills={[
-          // { iconName: 'css', label: 'CSS3' },
-          // { iconName: 'html', label: 'HTML5' },
-          { iconName: 'react', label: 'React' },
-          { iconName: 'javascript', label: 'Javascript' },
-          { iconName: 'typescript', label: 'Typescript' },
-          { iconName: 'nextjs', label: 'Nextjs' },
-          { iconName: 'sequelize', label: 'Sequelize' },
-          { iconName: 'git', label: 'Git' },
-          { iconName: 'nodejs', label: 'Nodejs' },
-          { iconName: 'express', label: 'Express' },
-        ]}
-      ></ProjectCard>
-      <ProjectCard
-        title={'MoneyMind'}
-        skills={[
-          // { iconName: 'css', label: 'CSS3' },
-          // { iconName: 'html', label: 'HTML5' },
-          { iconName: 'react', label: 'React' },
-          { iconName: 'javascript', label: 'Javascript' },
-          { iconName: 'typescript', label: 'Typescript' },
-          { iconName: 'nextjs', label: 'Nextjs' },
-          { iconName: 'sequelize', label: 'Sequelize' },
-          { iconName: 'git', label: 'Git' },
-          { iconName: 'nodejs', label: 'Nodejs' },
-          { iconName: 'express', label: 'Express' },
-        ]}
-      ></ProjectCard>
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          <ProjectCard
+            title={'MoneyMind'}
+            skills={[
+              // { iconName: 'css', label: 'CSS3' },
+              // { iconName: 'html', label: 'HTML5' },
+              { iconName: 'react', label: 'React' },
+              { iconName: 'javascript', label: 'Javascript' },
+              { iconName: 'typescript', label: 'Typescript' },
+              { iconName: 'nextjs', label: 'Nextjs' },
+              { iconName: 'sequelize', label: 'Sequelize' },
+              { iconName: 'git', label: 'Git' },
+              { iconName: 'nodejs', label: 'Nodejs' },
+              { iconName: 'express', label: 'Express' },
+            ]}
+          ></ProjectCard>
+          <ProjectCard
+            title={'MoneyMind'}
+            skills={[
+              // { iconName: 'css', label: 'CSS3' },
+              // { iconName: 'html', label: 'HTML5' },
+              { iconName: 'react', label: 'React' },
+              { iconName: 'javascript', label: 'Javascript' },
+              { iconName: 'typescript', label: 'Typescript' },
+              { iconName: 'nextjs', label: 'Nextjs' },
+              { iconName: 'sequelize', label: 'Sequelize' },
+              { iconName: 'git', label: 'Git' },
+              { iconName: 'nodejs', label: 'Nodejs' },
+              { iconName: 'express', label: 'Express' },
+            ]}
+          ></ProjectCard>
+          <ProjectCard
+            title={'MoneyMind'}
+            skills={[
+              // { iconName: 'css', label: 'CSS3' },
+              // { iconName: 'html', label: 'HTML5' },
+              { iconName: 'react', label: 'React' },
+              { iconName: 'javascript', label: 'Javascript' },
+              { iconName: 'typescript', label: 'Typescript' },
+              { iconName: 'nextjs', label: 'Nextjs' },
+              { iconName: 'sequelize', label: 'Sequelize' },
+              { iconName: 'git', label: 'Git' },
+              { iconName: 'nodejs', label: 'Nodejs' },
+              { iconName: 'express', label: 'Express' },
+            ]}
+          ></ProjectCard>
+        </div>
+      </div>
     </section>
   );
 };
