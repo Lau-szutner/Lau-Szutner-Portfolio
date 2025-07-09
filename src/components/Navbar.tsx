cbe2325d3173b8b49416cde5854da47291838bc4
Lau-szutner <lautaroszutner@gmail.com>
2025-07-09 19:21:59 -0300

    cambios de navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 const Navbar = () => {
   return (
-    <nav className="w-full z-50 flex justify-center bg-black">
-      <ul className="flex justify-center w-full text-2xl font-medium text-white w-10/12">
+    <nav className="w-full flex justify-center ">
+      <ul className="flex justify-center w-full text-1xl font-medium text-white w-10/12">
         {['Home', 'Projects', 'About'].map((item, idx) => (
           <li key={idx} className="flex-1 text-center">
-            <button className="w-full py-3 hover:bg-sky-500 hover:text-black transition duration-300">
+            <button className="w-full py-3 hover:bg-zinc-500  transition duration-300">
               <a href={`#${item}`}>{item}</a>
             </button>
           </li>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -10,33 +10,37 @@ const Projects = () => {
       <h2 className="text-4xl sm:text-6xl md:text-7xl font-bold text-center w-full mb-15">
         Projects
       </h2>
-      {/* <div className="grid grid-cols-2 gap-10">
-        <Project
-          title={'MoneyMind'}
-          skills={[
-            // { iconName: 'css', label: 'CSS3' },
-            // { iconName: 'html', label: 'HTML5' },
-            { iconName: 'react', label: 'React' },
-            { iconName: 'javascript', label: 'Javascript' },
-            { iconName: 'typescript', label: 'Typescript' },
-            { iconName: 'nextjs', label: 'Nextjs' },
-            { iconName: 'sequelize', label: 'Sequelize' },
-            { iconName: 'git', label: 'Git' },
-            { iconName: 'nodejs', label: 'Nodejs' },
-            { iconName: 'express', label: 'Express' },
-          ]}
-        />
-        <Project
-          title={'Mr Robot'}
-          skills={[
-            { iconName: 'css', label: 'CSS3' },
-            { iconName: 'html', label: 'HTML5' },
-            { iconName: 'javascript', label: 'Javascript' },
-            { iconName: 'git', label: 'Git' },
-          ]}
-        />
-      </div> */}
 
+      <ProjectCard
+        title={'MoneyMind'}
+        skills={[
+          // { iconName: 'css', label: 'CSS3' },
+          // { iconName: 'html', label: 'HTML5' },
+          { iconName: 'react', label: 'React' },
+          { iconName: 'javascript', label: 'Javascript' },
+          { iconName: 'typescript', label: 'Typescript' },
+          { iconName: 'nextjs', label: 'Nextjs' },
+          { iconName: 'sequelize', label: 'Sequelize' },
+          { iconName: 'git', label: 'Git' },
+          { iconName: 'nodejs', label: 'Nodejs' },
+          { iconName: 'express', label: 'Express' },
+        ]}
+      ></ProjectCard>
+      <ProjectCard
+        title={'MoneyMind'}
+        skills={[
+          // { iconName: 'css', label: 'CSS3' },
+          // { iconName: 'html', label: 'HTML5' },
+          { iconName: 'react', label: 'React' },
+          { iconName: 'javascript', label: 'Javascript' },
+          { iconName: 'typescript', label: 'Typescript' },
+          { iconName: 'nextjs', label: 'Nextjs' },
+          { iconName: 'sequelize', label: 'Sequelize' },
+          { iconName: 'git', label: 'Git' },
+          { iconName: 'nodejs', label: 'Nodejs' },
+          { iconName: 'express', label: 'Express' },
+        ]}
+      ></ProjectCard>
       <ProjectCard
         title={'MoneyMind'}
         skills={[
