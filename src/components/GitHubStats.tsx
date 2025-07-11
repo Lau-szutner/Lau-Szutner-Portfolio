701d54d61e0c3841b991975ebfd36639ea2fd368
Lau-szutner <lautaroszutner@gmail.com>
2025-07-11 00:31:16 -0300

    github stats

--- src/components/GitHubStats.tsx
+++ src/components/GitHubStats.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import Github from '../assets/github/github';
+import Arrow from '../assets/github/arrow';
 
 type GitHubStats = {
   repos: number;
@@ -26,6 +28,7 @@ export default function GitHubStats() {
       const reposRes = await fetch(userData.repos_url);
       const reposData = await reposRes.json();
 
+      console.log(reposData);
       const stars = reposData.reduce(
         (acc: number, repo: any) => acc + repo.stargazers_count,
         0
@@ -62,7 +65,11 @@ export default function GitHubStats() {
           </div>
 
           <div className="grid grid-cols-2 md:grid-cols-4 gap-6 text-center ">
-            <StatCard icon="🐙" value={stats.repos} label="Repositories" />
+            <StatCard
+              icon={<Github />}
+              value={stats.repos}
+              label="Repositories"
+            />
             <StatCard icon="⭐" value={stats.stars} label="Stars" />
             <StatCard icon="🍴" value={stats.forks} label="Forks" />
             <StatCard icon="📅" value={stats.commits} label="Commits" />
@@ -73,7 +80,9 @@ export default function GitHubStats() {
 
             <div className="relative z-10 bg-neutral-800 text-white p-5 rounded-md border-2 border-neutral-700 hover:border-[#00ffea]">
               <div className="flex items-center mb-4">
-                <span className="text-cyan-400 mr-2">📈</span>
+                <span className="text-cyan-400 mr-2">
+                  <Arrow></Arrow>
+                </span>
                 <h3 className="text-xl font-semibold">Contribution Activity</h3>
               </div>
 
@@ -97,16 +106,16 @@ export default function GitHubStats() {
             <div className="absolute inset-0 rounded-md bg-[#00ffea] opacity-0 group-hover:opacity-50 blur-lg transition-opacity duration-300 z-0"></div>
 
             <div className="relative z-10 bg-neutral-800 text-white p-5 rounded-md border-2 border-neutral-700 hover:border-[#00ffea]">
-              <div className="grid grid-cols-2 gap-6 mx-auto place-items-center ">
+              <div className="grid grid-cols-2 gap-6 mx-auto place-items-center">
                 <img
-                  src={`https://github-readme-stats.vercel.app/api?username=lau-szutner&show_icons=true&hide_border=true&title_color=4ade80&icon_color=4ade80&text_color=ffffff&bg_color=00000000`}
+                  src={`https://github-readme-stats.vercel.app/api?username=lau-szutner&show_icons=true&hide_border=true&title_color=ffffff&icon_color=00ffea&&text_color=ffffff&bg_color=00000000`}
                   alt="GitHub Stats"
-                  className="w-full"
+                  className="w-12/12"
                 />
                 <img
-                  src={`https://github-readme-stats.vercel.app/api/top-langs/?username=lau-szutner&layout=compact&hide_border=true&title_color=4ade80&icon_color=4ade80&text_color=ffffff&bg_color=00000000`}
+                  src={`https://github-readme-stats.vercel.app/api/top-langs/?username=lau-szutner&layout=compact&hide_border=true&title_color=ffffff&icon_color=4ade80&text_color=ffffff&bg_color=00000000&exclude_repo=BookUp,Cripto-Merch,Razer-Bootsrap`}
                   alt="Top Languages"
-                  className="w-full"
+                  className="w-11/12"
                 />
               </div>
             </div>
