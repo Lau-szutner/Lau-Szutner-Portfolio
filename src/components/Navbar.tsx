a6c2637c98762432e4c19638d39eed4b7e332b49
lau-szutner <lautaroszutner@gmail.com>
2025-08-20 18:41:55 -0300

    f

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="w-full flex justify-center ">
-      <ul className="flex justify-center w-full text-1xl font-medium text-white w-10/12">
+      <ul className="flex justify-center w-full text-1xl font-medium text-white ">
         {['Home', 'Projects', 'About', 'Github', 'Contact'].map((item, idx) => (
           <li key={idx} className="flex-1 text-center">
             <button className="w-full py-3 hover:bg-zinc-500  transition duration-300">
